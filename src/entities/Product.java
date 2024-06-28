40b1209aa88b3c294a62430c9e0d399d7339011b
enricogth <enrigazoth@gmail.com>
2024-06-28 16:05:55 -0300

    Declared lambda expression

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -26,10 +26,6 @@ public class Product {
 		this.price = price;
 	}
 
-	public void nonStaticPriceUpdate() {
-		price = price * 1.1;
-	}
-	
 	@Override
 	public String toString() {
 		return name + ", " + String.format("%.2f", price);
